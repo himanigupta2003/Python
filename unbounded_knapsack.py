56e98fbb8beffda77bb228493e0427c08b1e3f5c
Himani Gupta <73308203+himanigupta2003@users.noreply.github.com>
2021-09-01 16:39:19 +0530

    Updated_unbounded_knapsack
    did some minor changes

--- unbounded_knapsack.py
+++ unbounded_knapsack.py
@@ -7,8 +7,8 @@ from typing import List
 
 def unbounded_knapsack(capacity: int, weights: List[int], values: List[int], counter: int) -> int:
     """
-    Returns the maximum value that can be put in a unbounded_knapsack of a capacity cap,
-    whereby each weight w has a specific value val.
+    Returns the maximum value that can be put in a unbounded_knapsack of a capacity,
+    whereby each weight has a specific value.
 
    >>> capacity = 100
     >>> value = [10, 30, 20]
@@ -17,7 +17,7 @@ def unbounded_knapsack(capacity: int, weights: List[int], values: List[int], cou
     >>> unbounded_knapsack(capacity, weight, value, length)
     300
 
-    The result is 300 cause the values of 100 and 120 got the weight of 50
+    The result is 300 cause the values of 10 item of 30 got the weight of 300
     which is the limit of the capacity.
     """
 
@@ -34,8 +34,7 @@ def unbounded_knapsack(capacity: int, weights: List[int], values: List[int], cou
         return unbounded_knapsack(capacity, weights, values, counter - 1)
     else:
         left_capacity = capacity - weights[counter - 1]
-        new_value_included = values[counter - 1] + \
-            unbounded_knapsack(left_capacity, weights, values, counter)
+        new_value_included = values[counter - 1] + unbounded_knapsack(left_capacity, weights, values, counter)
         without_new_value = unbounded_knapsack(capacity, weights, values, counter - 1)
         return max(new_value_included, without_new_value)
 
